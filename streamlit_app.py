49e1728b1f19a9ba6145faa32ade1f25bcc1e163
Michael Moulsdale <michael@zoneone.co>
2023-04-24 22:50:28 +0100

    Update streamlit_app.py
    extra form to add fruit at the end

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,7 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+#add more fruit
+fruit_choice = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding ', fruit_choice)
