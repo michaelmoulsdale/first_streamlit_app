ed7c29eebe1dc61a7831962458b763fe16270ee8
Michael Moulsdale <michael@zoneone.co>
2023-04-24 21:46:54 +0100

    Update streamlit_app.py
    change to kiwi ready for variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ streamlit.dataframe(fruits_to_show)
 #new selection to display from fruity api services
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
 
 #makes the text readable
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
